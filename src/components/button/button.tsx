3c133712e4f4ae9f6d8213cc7fbf2714801b9115
Wiliam Traoré <hi@wil-liam.com>
2024-03-21 11:53:02 +0100

    feat(button): add gradient & rounded props

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -20,6 +20,8 @@ interface Props extends ComponentPropsWithoutRef<'button'> {
 	variant?: Variant;
 	link?: string;
 	linkOptions?: { external?: boolean; sameWindow?: boolean };
+	gradient?: boolean;
+	rounded?: boolean;
 }
 
 const sizeClasses: Record<Size, string> = {
@@ -27,7 +29,7 @@ const sizeClasses: Record<Size, string> = {
 	lg: 'h-13 px-5 rounded-md font-bold text-base',
 	base: 'h-10 px-3 rounded font-bold text-sm',
 	md: 'h-8 px-3 rounded font-bold text-sm',
-	sm: 'h-6 px-2 rounded-sm font-bold text-sm',
+	sm: 'h-6 px-2 rounded-sm font-bold text-xs',
 };
 
 const variantClasses: Record<Variant, string> = {
@@ -46,6 +48,14 @@ const variantClasses: Record<Variant, string> = {
 	light: 'bg-light-200 hover:bg-light-400 text-dark-700 outline-light-200',
 };
 
+const borderGradientSizeClasses: Record<Size, string> = {
+	xl: 'before:rounded-md',
+	lg: 'before:rounded-md',
+	base: 'before:rounded',
+	md: 'before:rounded',
+	sm: 'before:rounded-sm',
+};
+
 export function Button(props: Props) {
 	const {
 		children,
@@ -53,10 +63,24 @@ export function Button(props: Props) {
 		variant = 'primary',
 		link,
 		linkOptions,
+		gradient = false,
+		rounded = false,
 		...rest
 	} = props;
 
-	const buttonClasses = `transition-all duration-200 cursor-pointer focus-visible:outline-1 focus-visible:outline focus-visible:outline-offset-2 border-transparent border-solid inline-flex items-center ${sizeClasses[size]} ${variantClasses[variant]} ${rest.className}`;
+	const borderGradientClasses: Record<Variant, string> = {
+		primary: `relative border border-transparent bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-primary-400 to-primary-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		secondary: `relative border border-transparent bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-secondary-300 to-secondary-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		success: `relative border border-transparent bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-success-200 to-success-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		error: `relative border border-transparent bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-error-300 to-error-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		warning: `relative border border-transparent bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-warning-200 to-warning-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		stroke: `relative border border-dark-400 bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-dark-400 to-dark-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		transparent: ``,
+		dark: `relative border border-dark-400 bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-dark-400 to-dark-500 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+		light: `relative border border-light-400 bg-clip-padding before:content-[""] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:z-[-1] before:bg-gradient-to-b from-white to-light-400 before:m-[-1px] ${borderGradientSizeClasses[size]} ${rounded && '!before:rounded-full'}`,
+	};
+
+	const buttonClasses = `transition-all duration-200 cursor-pointer focus-visible:outline-1 focus-visible:outline focus-visible:outline-offset-2 border-transparent border-solid inline-flex items-center ${sizeClasses[size]} ${variantClasses[variant]} ${gradient && borderGradientClasses[variant]} ${rounded && '!rounded-full'} ${rest.className}`;
 
 	if (link && !linkOptions?.external) {
 		return (
