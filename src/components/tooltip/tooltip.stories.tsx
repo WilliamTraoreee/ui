9790a001c1a79c30c0add0617c4bb6be1b58dcc6
Wiliam Traoré <hi@wil-liam.com>
2024-03-25 00:01:05 +0100

    feat(tooltip): create component

---
+++ src/components/tooltip/tooltip.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Tooltip } from './tooltip';
+
+const meta = {
+	title: 'Tooltip',
+	component: Tooltip,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	argTypes: {},
+} satisfies Meta<typeof Tooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Component: Story = {
+	args: {
+		children: <p>Hover me</p>,
+		content: 'Tooltip content',
+	},
+};
---
+++ src/components/tooltip/tooltip.tsx
@@ -0,0 +1,44 @@
+import { Tooltip as TooltipPrimitive } from '@ark-ui/react';
+import { CSSProperties } from 'react';
+import { motion } from 'framer-motion';
+
+interface Props {
+	children: React.ReactNode;
+	content: string;
+}
+
+export function Tooltip(props: Props) {
+	const { children, content } = props;
+
+	return (
+		<TooltipPrimitive.Root lazyMount unmountOnExit openDelay={0}>
+			<TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+			<TooltipPrimitive.Positioner>
+				<motion.div
+					initial={{
+						y: 5,
+						opacity: 0,
+					}}
+					animate={{
+						y: 0,
+						opacity: 1,
+					}}
+				>
+					<TooltipPrimitive.Content className='text-sm bg-dark-800 rounded px-2 py-1'>
+						<TooltipPrimitive.Arrow
+							className='bg-dark-800 rotate-45 rounded-sm'
+							style={
+								{
+									'--arrow-size': '12px',
+								} as CSSProperties
+							}
+						>
+							<TooltipPrimitive.ArrowTip />
+						</TooltipPrimitive.Arrow>
+						{content}
+					</TooltipPrimitive.Content>
+				</motion.div>
+			</TooltipPrimitive.Positioner>
+		</TooltipPrimitive.Root>
+	);
+}
